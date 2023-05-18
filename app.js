de27c859cf253b8c64e2226c78bc8d43c3de93ac
Sumit Singh Tomar <111276093+Sumit-Si@users.noreply.github.com>
2023-05-18 18:46:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ menuBtn.addEventListener('click',() => {
 closeBtn.addEventListener('click',() => {
     navBar.style.right = "-20em";
 });
+
+const formCont = document.querySelector('.contact-cont');
+const formEl = document.querySelectorAll('.form-items');
+const subBtn = document.querySelector('input-btn');
+// console.log(formCont);
+
+formCont.addEventListener("click",(e)=> {
+
+    // console.log(e.target.name);
+    const inputName = e.target.name;
+
+    if(inputName) {
+        console.log("clicked");
+        formEl.forEach((item)=> {
+            // console.log(item);
+            item.classList.remove("live");
+        })
+    }
+}
+);
